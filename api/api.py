c3086dc14431963d7c9821ffdc3a8334f7b7ddf2
alex_chen_04 <127902759+Kloafe@users.noreply.github.com>
2026-02-21 17:59:14 -0500

    corsA

--- api/api.py
+++ api/api.py
@@ -1,8 +1,18 @@
 import time
 from flask import Flask
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/time')
 def get_current_time():
     return {'time': time.time()}
+
+
+@app.route('/api/dbquery')
+def db_query():
+    return {'message': "HELLO WORLD",
+            'data': [1,2,3,4,5]
+            }
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
+
+
 export default function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("http://127.0.0.1:5000/api/dbquery");
+      const data = await res.json();
+      setData(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <h1 className="text-2xl font-bold underline">
       Hello world!
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </h1>
+
   )
 }
