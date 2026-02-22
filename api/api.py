56cdddb61aede98916292ed97fe8d3373142d9e4
Alina <55455721+agonzalez22@users.noreply.github.com>
2026-02-21 21:21:53 -0500

    oh my

--- api/api.py
+++ api/api.py
@@ -1,18 +1,41 @@
 import time
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
+import os
+from supabase import create_client
+from flask_hot_reload import HotReload
 
+
+# CLIENNNTTT 
+url = os.environ.get("SUPABASE_URL")
+key = os.environ.get("SUPABASE_KEY")
+db = create_client(url, key)
 
 app = Flask(__name__)
 CORS(app)
+
+hot_reload = HotReload(app, includes=['templates', 'static', '.'], excludes=['__pycache__', 'node_modules', '.git'])
 
 @app.route('/api/time')
 def get_current_time():
     return {'time': time.time()}
 
 
-@app.route('/api/dbquery')
+@app.route('/test')
 def db_query():
-    return {'message': "HELLO WORLD",
-            'data': [1,2,3,4,5]
-            }
+    response = (
+    db.table("officers")
+    .select("*")
+    .execute()
+)
+    return {"message": response.data}
+
+@app.route('/officers')
+def get_officer_data():
+    """ 
+    SELECT * from officers where first_name='John' and last_name='Smith'
+    """
+    employee_id = request.args.get('employee_id')
+    response = db.table("officers").select("*").eq("Employee ID", employee_id).execute()
+    return {"message": response.data}
+
